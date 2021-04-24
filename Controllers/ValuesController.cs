f6096347bff3d92af333b529baf4d4fde0285e23
98842@stud.uz.zgora.pl <98842@stud.uz.zgora.pl>
2021-04-24 15:04:18 +0200

    Utworzenie projektu Portal Randkowy WEB API

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AplikacjaKurierska.API.Controllers
+{
+    //http:localhost:5000/api/Values
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        //GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>>Get()
+        {
+            return new string[]{"value1","value2","value3"};
+        }
+        
+        [HttpGet("{id}")]
+
+        public ActionResult<string>Get(int id){
+            return "value";
+        }
+
+
+    }
+
+}
+
