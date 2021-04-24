63a532dc0b7ad98bc1898be1bcb774eba3dabd25
98842@stud.uz.zgora.pl <98842@stud.uz.zgora.pl>
2021-04-24 15:24:08 +0200

    Dodanie SQLite

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+using AplikacjaKurierska.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AplikacjaKurierska.API.Data
+
+{
+    public class DataContext: DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext>options):base(options){}
+
+        public DbSet<Value>Values{get;set;}
+        
+    }
+}
